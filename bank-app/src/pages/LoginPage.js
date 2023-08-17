bdef6cb70323b9469dd410a7d6bbb293d25fc004
helmyrsln <amhm.work@gmail.com>
2023-08-17 17:02:09 +0800

    Added Link to Signup page

--- bank-app/src/pages/LoginPage.js
+++ bank-app/src/pages/LoginPage.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import '../styles/LoginPage.css'
+import { Link as RouterLink } from 'react-router-dom'
 
 export const LoginPage = () => (
-    <div className='app'>
-      <div className='login-form'>
-            <h1>LOGIN</h1>
-            <input type='text' id='email' placeholder='Email'></input>
-            <input type='text' id='pwd' placeholder='Password'></input>
-            <button>Login</button>
-            <p class="message">Not registered? <a href="">Create an account</a></p>
-            <p class="message">Forgot password? <a href="">Click here</a></p>
-      </div>
+  <div className="app">
+    <div className="login-form">
+      <h1>LOGIN</h1>
+      <input type="text" id="email" placeholder="Email"></input>
+      <input type="text" id="pwd" placeholder="Password"></input>
+      <button>Login</button>
+
+      <p class="message">
+        Not registered?
+        <RouterLink to="/register">
+          <a href="#"> Create an account</a>
+        </RouterLink>
+      </p>
+      <p class="message">
+        Forgot password?
+        <RouterLink to="/register">
+          <a href="#"> Click here</a>
+        </RouterLink>
+      </p>
     </div>
-  );
+  </div>
+);
