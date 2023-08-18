6b4af44ae7e788d2bfdc540c8b304fafbb8b9f36
helmyrsln <amhm.work@gmail.com>
2023-08-18 15:21:32 +0800

    Changes to Form -Ami

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 import "../styles/RegisterPage.css"; // same style as for Loginpage
 import { useAuth } from "../hooks/useAuth";
 
 export const RegisterPage = () => {
+  const navigate = useNavigate();
   const { addUser, login } = useAuth();
   const [confirmPassword, setConfirmPassword] = useState("");
   const [user, setUser] = useState({
@@ -23,7 +24,9 @@ export const RegisterPage = () => {
     if (confirmPassword === user.password) {
       addUser(user).then((created) => {
         if (created) {
-          login(user);
+          alert("User created");
+          login(user)
+          navigate("/login");
         } else {
           alert("User exists");
         }
@@ -64,95 +67,99 @@ export const RegisterPage = () => {
   return (
     <div className="app">
       <div className="login-form">
-        <h1>Reset Password</h1>
+        <h1>Register Account</h1>
 
         <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="email">Email</label>
             <input
               type="email"
               value={user.email}
               id="email"
               onChange={handleEmailChange}
+              placeholder="Email"
+              required
             ></input>
           </div>
           <div>
-            <label htmlFor="password">Password</label>
             <input
               type="password"
               value={user.password}
               onChange={handlePasswordChange}
               id="password"
+              placeholder="Password"
+              required
             ></input>
           </div>
           <div>
-            <label htmlFor="confirmPassword">Confirm Password</label>
             <input
               type="password"
               value={user.confirmPassword}
               onChange={handleConfirmPasswordChange}
               id="confirmPassword"
+              placeholder="Confirm Password"
+              required
             ></input>
           </div>
           <div>
-            <label htmlFor="firstName">First Name</label>
             <input
               type="text"
               value={user.firstName}
               onChange={handleFirstNameChange}
               id="firstName"
+              placeholder="First Name (optional)"
             ></input>
           </div>
           <div>
-            <label htmlFor="lastName">Last Name</label>
             <input
               type="text"
               value={user.lastName}
               onChange={handleLastChange}
               id="lastName"
+              placeholder="Last Name (optional)"
             ></input>
           </div>
           <div>
-            <label htmlFor="address">Address</label>
             <input
               type="text"
               value={user.address}
               onChange={handleAddressChange}
               id="address"
+              placeholder="Address (optional)"
             ></input>
           </div>
           <div>
-            <label htmlFor="contactNumber">Contact Number</label>
             <input
               type="text"
               value={user.contactNumber}
               onChange={handleContactNumberChange}
               id="contactNumber"
+              placeholder="Contact Number (optional)"
             ></input>
           </div>
           <div>
-            <label htmlFor="nric">NRIC</label>
             <input
               type="text"
               value={user.nric}
               onChange={handleNricChange}
               id="nric"
+              placeholder="NRIC (optional)"
             ></input>
           </div>
           <div>
-            <label htmlFor="dateOfBirth">Date of Birth</label>
             <input
-              type="date"
+              placeholder="Date of Birth (optional)"
               value={user.dateOfBirth}
               onChange={handleDateOfBirthChange}
               id="dateOfBirth"
+              onFocus={(e) => (e.target.type = "date")}
+              onBlur={(e) => (e.target.type = "text")}
             ></input>
           </div>
 
+          <button type="submit">Submit</button>
           <RouterLink to="/">
             <button className="secondary">Back</button>
           </RouterLink>
-          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
