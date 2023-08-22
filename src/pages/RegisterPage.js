fbf2f028fc42d14fae72c8d981c51a0112e3559e
helmyrsln <65496133+helmyrsln@users.noreply.github.com>
2023-08-22 09:47:39 +0800

    Latest Register Page (#4)
    * Changes to Form -Ami
    
    * Input requirement check added
    
    * delete login(user)
    
    ---------
    
    Co-authored-by: SuperiormonLBJ <superiormon187@gmail.com>

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 import "../styles/RegisterPage.css"; // same style as for Loginpage
 import { useAuth } from "../hooks/useAuth";
 
 export const RegisterPage = () => {
+  const navigate = useNavigate();
   const { addUser, login } = useAuth();
   const [confirmPassword, setConfirmPassword] = useState("");
   const [user, setUser] = useState({
@@ -18,18 +19,57 @@ export const RegisterPage = () => {
     dateOfBirth: "",
   });
 
+  // input check
+  function requirement_check() {
+    // password
+    if (confirmPassword !== user.password) {
+      alert("Passwords do not match!");
+      return false;
+    }
+    if (user.password.length <= 4) {
+      alert("Password too short!");
+      return false;
+    }
+    if (!user.password.match(/[A-Z]/g)) {
+      alert("Include at least 1 uppercase!");
+      return false;
+    }
+
+    // invalid nric check
+    if (user.nric.length < 4) {
+      alert("Please enter a valid NRIC number!");
+      return false;
+    }
+    // adult for registration
+    if (!user.dateOfBirth) {
+      alert("Please enter a valid birthday!");
+      return false;
+    }
+    const currentDate = new Date();
+    const birthday = new Date(user.dateOfBirth);
+    var timsedifference =
+      (currentDate - birthday) / (1000 * 60 * 60 * 24 * 365);
+    console.log("this is timediff", timsedifference);
+    if (timsedifference < 18) {
+      alert("No kids, this is an adult webpage!");
+      return false;
+    }
+
+    return true;
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (confirmPassword === user.password) {
+
+    if (requirement_check()) {
       addUser(user).then((created) => {
         if (created) {
-          login(user);
+          alert("User created!");
+          navigate("/");
         } else {
-          alert("User exists");
+          alert("User NRIC exists");
         }
       });
-    } else {
-      alert("passwords do not match");
     }
   };
 
@@ -64,95 +104,100 @@ export const RegisterPage = () => {
   return (
     <div className="app">
       <div className="login-form">
-        <h1>Reset Password</h1>
+        <h1>Register Account</h1>
+        <h5>*: Required Fields</h5>
 
         <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="email">Email</label>
             <input
               type="email"
               value={user.email}
               id="email"
               onChange={handleEmailChange}
+              placeholder="Email*"
+              required
             ></input>
           </div>
           <div>
-            <label htmlFor="password">Password</label>
             <input
               type="password"
               value={user.password}
               onChange={handlePasswordChange}
               id="password"
+              placeholder="Password* (Min. 4 digit & 1 uppercase)"
+              required
             ></input>
           </div>
           <div>
-            <label htmlFor="confirmPassword">Confirm Password</label>
             <input
               type="password"
               value={user.confirmPassword}
               onChange={handleConfirmPasswordChange}
               id="confirmPassword"
+              placeholder="Confirm Password*"
+              required
             ></input>
           </div>
           <div>
-            <label htmlFor="firstName">First Name</label>
             <input
               type="text"
               value={user.firstName}
               onChange={handleFirstNameChange}
               id="firstName"
+              placeholder="First Name (optional)"
             ></input>
           </div>
           <div>
-            <label htmlFor="lastName">Last Name</label>
             <input
               type="text"
               value={user.lastName}
               onChange={handleLastChange}
               id="lastName"
+              placeholder="Last Name (optional)"
             ></input>
           </div>
           <div>
-            <label htmlFor="address">Address</label>
             <input
               type="text"
               value={user.address}
               onChange={handleAddressChange}
               id="address"
+              placeholder="Address (optional)"
             ></input>
           </div>
           <div>
-            <label htmlFor="contactNumber">Contact Number</label>
             <input
               type="text"
               value={user.contactNumber}
               onChange={handleContactNumberChange}
               id="contactNumber"
+              placeholder="Contact Number (optional)"
             ></input>
           </div>
           <div>
-            <label htmlFor="nric">NRIC</label>
             <input
               type="text"
               value={user.nric}
               onChange={handleNricChange}
               id="nric"
+              placeholder="NRIC* (at least 4 digit)"
             ></input>
           </div>
           <div>
-            <label htmlFor="dateOfBirth">Date of Birth</label>
             <input
-              type="date"
+              placeholder="Date of Birth* (More than 18 year-old)"
               value={user.dateOfBirth}
               onChange={handleDateOfBirthChange}
               id="dateOfBirth"
+              onFocus={(e) => (e.target.type = "date")}
+              onBlur={(e) => (e.target.type = "text")}
             ></input>
           </div>
 
+          <button type="submit">Submit</button>
           <RouterLink to="/">
             <button className="secondary">Back</button>
           </RouterLink>
-          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
