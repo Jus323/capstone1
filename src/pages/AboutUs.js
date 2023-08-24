c2b844dcf0ad8f83af1c88bddb6395a4a0648549
mushroomyyy <jordanvckj@gmail.com>
2023-08-24 09:35:28 +0800

    made it more legit

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,14 +2,14 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { useNavigate } from "react-router-dom";
-import "../styles/LoginPage.css"; // same style as for Loginpage
 import blue from "../img/blue.png";
-import red from "../img/red.png";
-import pink from "../img/pink.png";
 import green from "../img/green.png";
-import yellow from "../img/yellow.png";
-import white from "../img/white.png";
 import orange from "../img/orange.png";
+import pink from "../img/pink.png";
+import red from "../img/red.png";
+import white from "../img/white.png";
+import yellow from "../img/yellow.png";
+import "../styles/LoginPage.css"; // same style as for Loginpage
 
 export const AboutUsPage = () => {
   const navigate = useNavigate();
@@ -20,15 +20,17 @@ export const AboutUsPage = () => {
 
   return (
     <div>
-      <h1>Among Sridhar</h1>
+      <h1>
+        {" "}
+        <s>About Us </s> Among Us
+      </h1>
       <div>
         <Card className="card-with-spacing">
           <Card.Img variant="top" src={green} />
           <Card.Body>
-            <Card.Title>Jordan</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+            <Card.Title className="cardtitle">Jordan</Card.Title>
+            <Card.Text className="carddesc">
+              -Profile Edit / About Us Specialist
             </Card.Text>
             <Button variant="primary">
               Focused on the Profile Editing Page with Pink and this About Us
@@ -40,10 +42,9 @@ export const AboutUsPage = () => {
         <Card className="card-with-spacing">
           <Card.Img variant="top" src={blue} />
           <Card.Body>
-            <Card.Title>Bhargav</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+            <Card.Title className="cardtitle">Bhargav</Card.Title>
+            <Card.Text className="carddesc">
+              -Routing Specialist / Logic Analyst / Debugging Specialist
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
@@ -52,10 +53,9 @@ export const AboutUsPage = () => {
         <Card className="card-with-spacing">
           <Card.Img variant="top" src={orange} />
           <Card.Body>
-            <Card.Title>Amirul</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+            <Card.Title className="cardtitle">Amirul</Card.Title>
+            <Card.Text className="carddesc">
+              -Main CSS Styling Expert / Forgot Password Page Specialist
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
@@ -64,11 +64,8 @@ export const AboutUsPage = () => {
         <Card className="card-with-spacing">
           <Card.Img variant="top" src={pink} />
           <Card.Body>
-            <Card.Title>Justin</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
+            <Card.Title className="cardtitle">Justin</Card.Title>
+            <Card.Text className="carddesc">-Profile Edit Masterist</Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
         </Card>{" "}
@@ -80,10 +77,9 @@ export const AboutUsPage = () => {
         <Card className="card-with-spacing">
           <Card.Img variant="top" src={yellow} />
           <Card.Body>
-            <Card.Title>Bei Ji</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+            <Card.Title className="cardtitle">BeiJi</Card.Title>
+            <Card.Text className="carddesc">
+              -Registration Page Specialist
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
@@ -92,22 +88,16 @@ export const AboutUsPage = () => {
         <Card className="card-with-spacing">
           <Card.Img variant="top" src={red} />
           <Card.Body>
-            <Card.Title>Wei Feng</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
+            <Card.Title className="cardtitle">Wei Feng</Card.Title>
+            <Card.Text className="carddesc">-HomePage Masterist</Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
         </Card>{" "}
         <Card className="card-with-spacing">
           <Card.Img variant="top" src={white} />
           <Card.Body>
-            <Card.Title>Roy</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
+            <Card.Title className="cardtitle">Roy</Card.Title>
+            <Card.Text className="carddesc">-Navigation Bar Analyst</Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
         </Card>{" "}
